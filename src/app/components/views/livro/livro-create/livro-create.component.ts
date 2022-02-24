91395104052cb8e1015b63c1d92ed097db34ef0f
Julio Freitas <juliosparapan2014@gmail.com>
2022-02-24 17:46:22 -0300

    Criado Livro-create

--- src/app/components/views/livro/livro-create/livro-create.component.ts
+++ src/app/components/views/livro/livro-create/livro-create.component.ts
@@ -1,3 +1,6 @@
+import { Livro } from './../livro.model';
+import { ActivatedRoute, Router } from '@angular/router';
+import { LivroService } from './../livro.service';
 import { Component, OnInit } from '@angular/core';
 import { FormControl, Validators } from '@angular/forms';
 
@@ -8,13 +11,33 @@ import { FormControl, Validators } from '@angular/forms';
 })
 export class LivroCreateComponent implements OnInit {
 
+  id_cat: String = ''
+
+  livro: Livro = {
+    id: '',
+    titulo: '',
+    nome_autor: '',
+    texto: ''
+  }
+
   titulo = new FormControl('', [Validators.minLength(3)])
   nome_autor = new FormControl('', [Validators.minLength(3)])
   texto = new FormControl('', [Validators.minLength(10)])
 
-  constructor() { }
+  constructor(private service: LivroService, private route: ActivatedRoute, private router: Router) { }
 
   ngOnInit(): void {
+    this.id_cat = this.route.snapshot.paramMap.get('id_cat')!
+  }
+
+  create(): void {
+    this.service.create(this.livro, this.id_cat).subscribe((responsta) => {
+    this.router.navigate([`categorias/${this.id_cat}/livros`]);
+    this.service.mensagem('Livro criado com sucesso !');
+    }, err => {
+      this.router.navigate([`categorias/${this.id_cat}/livros`]);
+      this.service.mensagem('Erro ao crair Livro !');
+    })
   }
 
   getMessage() {
